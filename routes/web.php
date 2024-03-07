d5fafc8f1d7ee2663e1d316edd5635409832a633
titokawa <titokawa.biz@gmail.com>
2024-03-07 14:57:50 +0900

    make player model

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Model;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +18,7 @@ use Illuminate\Support\Facades\Model;
 */
 
 Route::get('/', function () {
-    return 'test3';
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -26,6 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('player', PlayerController::class);
 });
 
 require __DIR__.'/auth.php';

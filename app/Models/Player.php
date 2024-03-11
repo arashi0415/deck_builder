cfadb3ab15b19f6bf1a07b76e6c5e12d71a74046
arashi0415 <arachi0415@gmail.com>
2024-03-11 13:53:15 +0900

    playerページバックエンド

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,22 +4,45 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\PlayerController;
 
 class Player extends Model
 {
     protected $guarded = [
-        "user_id"
+        "user_id",
     ];
 
     protected $rules = [
         'name' => 'required|string|max:255',
         'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:8|confirmed',
         'introduction' => 'nullable', 'string', 'max:1000',
-        'icon' => 'required', 'image', 'max:1024', 'unique:players',
-        'Favorite_card' => 'required', 'image', 'max:1024', 'unique:players',
+        'icon' => 'required|image|max:1024',
+        'favorite_card' => 'required|image|max:1024',
         'tag' => 'nullable', 'string', 'max:255', 'regex:/^[^,]+\,?(?<!,)$/'
-      ];
+    ];
+
+    protected $fillable = [
+        'name',
+        'email',
+        'introduction',
+        'icon',
+        'favorite_card',
+        'tag',
+        "registered"
+    ];
+
+
+    protected $casts = [
+        'registered' => 'boolean',
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->registered = true;
+    }
+
 
 
 
